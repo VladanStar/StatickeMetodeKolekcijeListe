f567c59734b52808a14dde0632dfe838d4b4e626
Vladan Cupric <47221439+VladanStar@users.noreply.github.com>
2020-07-13 23:26:23 +0200

    Add files via upload

--- StatickeMetodeListeKolekcije/src/com/company/Main.java
+++ StatickeMetodeListeKolekcije/src/com/company/Main.java
@@ -1,9 +1,7 @@
 package com.company;
 
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -29,8 +27,50 @@ public class Main {
         List<String> list3 = Arrays.asList("blue","green","red");
         System.out.println("(3) Index: " + Collections.binarySearch(list3, "red"));
         System.out.println("(4) Index: "+ Collections.binarySearch(list3,"cyan"));
-        
 
+        // obrnuti red elemenata u listi
+
+        List<Integer> list4 = Arrays.asList(1,2,3,4,5,6,7);
+        Collections.reverse(list4);
+        System.out.println(list4);
+
+        // redosled elemenata na slucajan nacin
+        List<String> list5 = Arrays.asList("Vladan","Milos","Aleksandar","Zoran","Milutin");
+        Collections.shuffle(list5);
+        System.out.println(list5);
+
+        // redosled elemenata na slucajan nacin, dve liste
+
+
+        List<String> list6 = Arrays.asList("red","green","yellow","blue");
+        List<String> list7 = Arrays.asList("green","yellow","blue","red");
+        Collections.shuffle(list7,new Random(20));
+        Collections.shuffle(list6,new Random(20));
+        System.out.println(list7);
+        System.out.println(list6);
+
+
+        //kopiranje u listu sa istim indeksom
+        List<String> list8 = Arrays.asList("yellow","red","green","blue");
+        List<String> list9 = Arrays.asList("white","black");
+        Collections.copy(list8,list9);
+        System.out.println(list8);
+
+        // nepromenljiva lista koja ima n kopija
+        List<GregorianCalendar> list10 =Collections.nCopies(5, new GregorianCalendar(2005,0,1));
+
+        // zamena elemenata specificiranim elementima
+        List<String> list11 = Arrays.asList("red","green","blue");
+        Collections.fill(list11,"black");
+        System.out.println(list11);
+
+        // metod disjoint vraca true ako liste nemaju zajednickih elemenata
+
+        Collection<String> collection1 = Arrays.asList("Vladan","Zoran","Predrag");
+        Collection<String> collection2 = Arrays.asList("Burovic","Markovic","Mangel");
+        Collection<String> collection3 = Arrays.asList("Jelena","Mirjana","Marija");
+        System.out.println(Collections.disjoint(collection1,collection2));
+        System.out.println(Collections.disjoint(collection2,collection3));
 
 
     }
